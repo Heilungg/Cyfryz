a74768647846e132f42c8533a3ebe68180b13df0
Wiktor <wiktorbabis@gmail.com>
2022-10-28 00:06:13 +0200

    Update cyf.R

--- cyf.R
+++ cyf.R
@@ -6,8 +6,8 @@ library("mlbench")
 library("caret")
 
 data(Sonar)
-
-set.seed(107)
+#baza tutoriala
+set.seed(123)
 
 inTrain <- createDataPartition(
   y = Sonar$Class,
@@ -20,11 +20,29 @@ str(inTrain)
 training <- Sonar[ inTrain,]
 testing  <- Sonar[-inTrain,]
 
+#ctrl
+ctrl <- trainControl(
+  method = "repeatedcv", 
+  repeats = 3,
+  classProbs = TRUE, 
+  summaryFunction = twoClassSummary
+)
+
+#ROC stat
 plsFit <- train(
   Class ~ .,
   data = training,
   method = "pls",
-  preProc = c("center", "scale")
+  preProc = c("center", "scale"),
+  tuneLength = 15,
+  trControl = ctrl,
+  metric = "ROC"
 )
 
+plsFit
 
+ggplot(plsFit)
+
+#confusin matrix
+plsClasses <- predict(plsFit, newdata = testing)
+confusionMatrix(data = plsClasses, testing$Class)
